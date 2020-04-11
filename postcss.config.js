ba6bf29a2abbc0b840080304d27583753a436f6b
mengff <mengff@mengffdeMacBook-Pro.local>
2020-04-11 15:42:48 +0800

    移除postcss的rem插件

--- postcss.config.js
+++ postcss.config.js
@@ -4,10 +4,6 @@ var env = process.env.NODE_ENV;
 var aPlugin = [
 	require('autoprefixer')({ browsers: ['> 5%', 'ie 9'] })
 ];
-if (config.enableRem) {
-	aPlugin.push(require('postcss-plugin-px2rem')(config.px2remOptions));
-	aPlugin.push(require('postcss-flexible')(config.px2rem));
-}
 if (env == 'production') {
 	aPlugin.push(require('postcss-clean')({}));
 }
